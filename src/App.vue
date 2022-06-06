34ea259cc659b66a07ffa61b02d45e38107bd51c
Andrew Gavgavian <andrewgee7@gmail.com>
2022-06-05 22:40:43 -0700

    whack

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
+<HeaderBar></HeaderBar>
   <p>Welcome to my website</p>
 </template>
 
 <script>
+import HeaderBar from "./components/HeaderBar.vue";
 
 export default {
   name: 'App',
   components: {
+    HeaderBar
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "@/assets/styles/_variables.scss";
 </style>
